6aa993b82678e0a5a93d4d3cb760102171a7d031
blu133721 <tiganinpungasrl@gmail.com>
2023-12-02 16:17:47 +0200

    Testing.

--- src/main.cpp
+++ src/main.cpp
@@ -26,8 +26,28 @@ void OnProgramLoad(const char *pluginName, const char *mainFilePath)
     timers = new Timers();
 }
 
+void Command_Test(int playerID, const char **args, uint32_t argsCount, bool silent)
+{
+        Player *player = g_playerManager->GetPlayer(playerID);
+        if (!player)
+            return;
+
+        player->SendMsg(HUD_PRINTTALK, "[1TAP] -----------------------------------------------------");
+        player->SendMsg(HUD_PRINTTALK, "Rule 1: "config->Fetch<const char*>("rules.rule1"),);
+        player->SendMsg(HUD_PRINTTALK, "Rule 2: "config->Fetch<const char*>("rules.rule1"),);
+        player->SendMsg(HUD_PRINTTALK, "Rule 3: "config->Fetch<const char*>("rules.rule1"),);
+        player->SendMsg(HUD_PRINTTALK, "Rule 4: "config->Fetch<const char*>("rules.rule1"),);
+        player->SendMsg(HUD_PRINTTALK, "Rule 5: "config->Fetch<const char*>("rules.rule1"),);
+        player->SendMsg(HUD_PRINTTALK, "Rule 6: "config->Fetch<const char*>("rules.rule1"),);
+        player->SendMsg(HUD_PRINTTALK, "Rule 7: "config->Fetch<const char*>("rules.rule1"),);
+        player->SendMsg(HUD_PRINTTALK, "Rule 8: "config->Fetch<const char*>("rules.rule1"),);
+        return;
+}
+
 void OnPluginStart()
 {
+    commands->Register("!rules", reinterpret_cast<void *>(&Command_Test));
+    commands->Register("!reguli", reinterpret_cast<void *>(&Command_Test));
 }
 
 void OnPluginStop()
